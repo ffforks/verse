e631ad09172f1dd24d0f4c63211aa9fc31918485
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-03-03 09:58:14 +0000

    Editing
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@12 49c17534-1d02-dc11-b283-0030488c597c

--- vs_main.c
+++ vs_main.c
@@ -1,5 +1,5 @@
 /*
-**
+** A simple Verse server.
 */
 
 #include <stdio.h>
@@ -7,13 +7,13 @@
 
 #include "v_cmd_gen.h"
 
-#if !defined(V_GENERATE_FUNC_MODE)
+#if !defined V_GENERATE_FUNC_MODE
 
 #include "verse.h"
 #include "vs_server.h"
 
-extern VNodeID vs_node_create(VNodeID owner_id, unsigned int type);
-extern void callback_send_node_destroy(void *user_data, VNodeID node_id);
+extern VNodeID	vs_node_create(VNodeID owner_id, unsigned int type);
+extern void	callback_send_node_destroy(void *user_data, VNodeID node_id);
 
 static void callback_send_connect(void *user, char *name, char *pass, void *address)
 {
@@ -50,14 +50,16 @@ int main(int argc, char **argv)
 	vs_c_callback_init();
 	vs_h_callback_init();
 	init_callback_node_storage();
-	verse_callback_set(verse_send_connect, callback_send_connect, NULL);
-	verse_callback_set(verse_send_ping, callback_send_ping, NULL);
+	verse_callback_set(verse_send_connect,		callback_send_connect,		NULL);
+	verse_callback_set(verse_send_ping,		callback_send_ping,		NULL);
 	verse_callback_set(verse_send_connect_terminate, callback_send_connect_terminate, NULL);
+
 	while(TRUE)
 	{
 		vs_set_next_session();
 		verse_callback_update(10);
 	}
+	return EXIT_SUCCESS;
 }
 
 #endif		/* V_GENERATE_FUNC_MODE */
