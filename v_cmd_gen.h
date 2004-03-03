2f1069d7d23ee735f95a049110dc8bf9348a4979
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-03-03 14:50:07 +0000

    Minor editing
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@26 49c17534-1d02-dc11-b283-0030488c597c

--- v_cmd_gen.h
+++ v_cmd_gen.h
@@ -1,7 +1,11 @@
+/*
+**
+*/
 
+/* Define this to at least build the code that regenerates the variable parts of the code. */
 /*#define V_GENERATE_FUNC_MODE*/
 
-typedef enum{
+typedef enum {
 	VCGP_UINT8,
 	VCGP_UINT16,
 	VCGP_UINT32,
@@ -20,20 +24,18 @@ typedef enum{
 	VCGP_PACK_INLINE,
 	VCGP_UNPACK_INLINE,
 	VCGP_END_ADDRESS
-}VCGParam;
+} VCGParam;
 
-typedef enum{
+typedef enum {
 	VCGCT_NORMAL,
 	VCGCT_UNIQUE,
 	VCGCT_ONCE,
-	VCGCT_INVISIBLE_SYSTEM, /* in the dark we are all invisible */
+	VCGCT_INVISIBLE_SYSTEM,	/* In the dark we are all invisible. */
 	VCGCT_ORDERED
-}VCGCommandType;
+} VCGCommandType;
 
 extern void v_cg_new_cmd(VCGCommandType type, char *name, unsigned int cmd_id, VCGCommandType command);
 extern void v_cg_add_param(VCGParam type, char *name);
 extern void v_cg_alias(char bool_switch, char *name, char *qualifyer, unsigned int param, unsigned int *param_array);
 extern void v_cg_end_cmd(void);
 extern void v_cg_new_manual_cmd(unsigned int cmd_id, char *name, char *params, char *alias_name, char *alias_params);
-
-
