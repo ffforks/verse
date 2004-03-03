096cb3e8dc5f102666dc8d6c234c4b84d1755556
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-03-03 10:15:46 +0000

    Whitespace changes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@18 49c17534-1d02-dc11-b283-0030488c597c

--- v_cmd_buf.h
+++ v_cmd_buf.h
@@ -1,53 +1,58 @@
+/*
+**
+*/
+
 #include <stdlib.h>
+
 #include "verse_header.h"
 #include "v_pack.h"
 
-typedef enum{
+typedef enum {
 	VCMDBS_10 = 0,
 	VCMDBS_50 = 1,
 	VCMDBS_100 = 2,
 	VCMDBS_500 = 3,
 	VCMDBS_1500 = 4,
 	VCMDBS_COUNT = 5
-}VCMDBufSize;
+} VCMDBufSize;
 
-typedef struct{
-	void			*next;
-	uint32			packet;
+typedef struct {
+	void		*next;
+	uint32		packet;
 	unsigned int	address_size;
 	unsigned int	address_sum;
-	VCMDBufSize		buf_size;
+	VCMDBufSize	buf_size;
 	unsigned int	size;
-}VCMDBufHead;
+} VCMDBufHead;
 
-typedef struct{
-	VCMDBufHead head;
+typedef struct {
+	VCMDBufHead	head;
 	uint8		buf[10];
-}VCMDBuffer10;
+} VCMDBuffer10;
 
-typedef struct{
-	VCMDBufHead head;
+typedef struct {
+	VCMDBufHead	head;
 	uint8		buf[50];
-}VCMDBuffer50;
+} VCMDBuffer50;
 
-typedef struct{
+typedef struct {
 	VCMDBufHead head;
 	uint8		buf[100];
-}VCMDBuffer100;
+} VCMDBuffer100;
 
-typedef struct{
-	VCMDBufHead head;
+typedef struct {
+	VCMDBufHead	head;
 	uint8		buf[500];
-}VCMDBuffer500;
+} VCMDBuffer500;
 
-typedef struct{
-	VCMDBufHead head;
+typedef struct {
+	VCMDBufHead	head;
 	uint8		buf[1500];
-}VCMDBuffer1500;
+} VCMDBuffer1500;
 
 extern VCMDBufHead	*v_cmd_buf_allocate(VCMDBufSize buf_size);
-extern void			v_cmd_buf_free(VCMDBufHead *head);
+extern void		v_cmd_buf_free(VCMDBufHead *head);
 
-extern void			v_cmd_buf_set_address_size(VCMDBufHead *head, unsigned int address_size, unsigned int size);
-extern void			v_cmd_buf_set_uniqe_size(VCMDBufHead *head, unsigned int size);
+extern void		v_cmd_buf_set_address_size(VCMDBufHead *head, unsigned int address_size, unsigned int size);
+extern void		v_cmd_buf_set_uniqe_size(VCMDBufHead *head, unsigned int size);
 extern boolean		v_cmd_buf_compare(VCMDBufHead *a, VCMDBufHead *b);
