3e0bc15d1c1e1fb391bb04002cb120f1d8420fdc
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-06-11 14:54:36 +0000

    R2: New ordered command for text setting
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk/verse@195 49c17534-1d02-dc11-b283-0030488c597c

--- v_cmd_def_t.c
+++ v_cmd_def_t.c
@@ -31,15 +31,15 @@ void v_gen_text_cmd_def(void)
 	v_cg_alias(TRUE, "t_buffer_unsubscribe", NULL, 2, NULL);
 	v_cg_end_cmd();
 
+	v_cg_new_manual_cmd(99, "t_text_set", "void verse_send_t_text_set(VNodeID node_id, VNMBufferID buffer_id, uint32 pos, uint16 length, const char *text)", NULL, NULL);
+/*
 	v_cg_new_cmd(V_NT_TEXT,			"t_line_insert", 99, VCGCT_ORDERED);
 	v_cg_add_param(VCGP_NODE_ID,	"node_id");
 	v_cg_add_param(VCGP_BUFFER_ID,	"buffer_id");
 	v_cg_add_param(VCGP_UINT32,		"line");
-	v_cg_add_param(VCGP_UINT16,		"index");
 	v_cg_add_param(VCGP_LONG_NAME,	"text");
 	v_cg_alias(FALSE, "t_line_delete", "if(text[0] == 0)", 3, NULL);
-
-	v_cg_end_cmd();
+	v_cg_end_cmd();*/
 }
 
 #endif
