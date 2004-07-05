735e1f346f19f613788779e6366f8149655f0f59
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-05 08:53:21 +0000

    Merge from r2p5
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@241 49c17534-1d02-dc11-b283-0030488c597c

--- v_connection.c
+++ v_connection.c
@@ -30,11 +30,11 @@ typedef struct {
 	void		*ordered_storage;
 } VConnection;
 
-struct {
+static struct {
 	VConnection	*con;
 	unsigned int	con_count;
 	unsigned int	current_connection;
-	void		*connect_address;
+	VNetworkAddress	*connect_address;
 	void		*unified_func_storage;
 	uint16		connect_port;
 } VConData;
@@ -84,6 +84,10 @@ VNetworkAddress * v_con_connect(const char *address)
 	VConData.current_connection = VConData.con_count;
 	++VConData.con_count;
 
+	/* FIXME: Without this buffering, clients create a new socket on callback_update(). Is this right? */
+	if(VConData.connect_address == NULL)
+		VConData.connect_address = net;
+
 	return net;
 }
 
@@ -113,7 +117,7 @@ void * v_con_get_network_queue(void)
 	return VConData.con[VConData.current_connection].network_queue;
 }
 
-void * v_con_get_network_address(void)
+VNetworkAddress * v_con_get_network_address(void)
 {
 	return VConData.con[VConData.current_connection].network_address;
 }
@@ -135,10 +139,9 @@ uint32 verse_session_get_avatar(void)
 
 void *v_con_get_network_address_id(unsigned int id)
 {
-	if(id < VConData.con_count)
+	if(VConData.con_count > 0 && id < VConData.con_count)
 		return VConData.con[VConData.current_connection].network_address;
-	else
-		return VConData.connect_address;
+	return VConData.connect_address;
 }
 
 unsigned int v_con_get_network_address_count(void)
@@ -152,7 +155,7 @@ unsigned int v_con_get_network_address_count(void)
 
 void v_con_network_listen(void)
 {
-	void *address;
+	VNetworkAddress *address;
 	char buf[V_MAX_CONNECT_PACKET_SIZE];
 
 	/* no mid send new work looking for now */
@@ -185,7 +188,7 @@ void verse_callback_update(unsigned int milliseconds)
 		if(VConData.connect_address == NULL)
 			VConData.connect_address = v_n_create_network_address(VERSE_STD_CONNECT_PORT/*v_connect_port*/, "localhost");
 		v_n_wait_for_incoming(milliseconds);
-		size = v_n_receive_data(VConData.connect_address, buf, V_MAX_CONNECT_PACKET_SIZE, FALSE);
+		size = v_n_receive_data(VConData.connect_address, buf, sizeof buf, FALSE);
 		if(size != -1)
 		{
 			char string[200];
@@ -201,7 +204,7 @@ void verse_callback_update(unsigned int milliseconds)
 		{
 			if(!v_fs_buf_unpack_stored() || !v_fs_func_accept_connections())
 				v_n_wait_for_incoming(milliseconds);
-			size = v_n_receive_data(address, buf, V_MAX_CONNECT_PACKET_SIZE, TRUE);
+			size = v_n_receive_data(address, buf, sizeof buf, TRUE);
 			if(size != -1 || size != 0)
 				VConData.con[VConData.current_connection].ping_timeout += milliseconds;
 			while(size != -1 && size != 0)
@@ -209,7 +212,7 @@ void verse_callback_update(unsigned int milliseconds)
 				VConData.con[VConData.current_connection].ping_timeout = 0;
 				VConData.con[VConData.current_connection].connect_timeout = 0;
 				v_fs_buf_unpack(buf, size);
-				size = v_n_receive_data(address, buf, V_MAX_CONNECT_PACKET_SIZE, TRUE);
+				size = v_n_receive_data(address, buf, sizeof buf, TRUE);
 			}
 			if(VConData.con[VConData.current_connection].ping_timeout > PING_TIMEOUT)
 			{
