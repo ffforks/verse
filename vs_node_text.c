afbcd970d7a28e6e78b203cf31b09f6c6500828e
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-05 09:22:37 +0000

    Text storage implemented for R3
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@247 49c17534-1d02-dc11-b283-0030488c597c

--- vs_node_text.c
+++ vs_node_text.c
@@ -12,9 +12,12 @@
 #include "verse.h"
 #include "vs_server.h"
 
+#define VS_TEXT_CHUNK_SIZE 4096
+
 typedef struct {
-	char			name[16];
-	char			**lines;
+	char				name[16];
+	char				*text;
+	unsigned int		length;
 	unsigned int		allocated;
 	VSSubscriptionList	*subscribers;
 } VSTextBuffer;
@@ -46,16 +49,14 @@ VSNodeText * vs_t_create_node(unsigned int owner)
 
 void vs_t_destroy_node(VSNodeText *node)
 {
-	unsigned int i, j;
+	unsigned int i;
 	destroy_node_head(&node->head);
 	free(node);
 	for(i = 0; i < node->buffer_count; i++)
 	{
 		if(node->buffer[i].name[0] != 0)
 		{
-			for(j = 0; j < node->buffer[i].allocated && node->buffer[i].lines[j] != NULL; j++)
-				free(node->buffer[i].lines[j]);
-			free(node->buffer[i].lines);
+			free(node->buffer[i].text);
 			vs_destroy_subscription_list(node->buffer[i].subscribers);
 		}
 	}
@@ -117,23 +118,23 @@ static void callback_send_t_buffer_create(void *user, VNodeID node_id, VNMBuffer
 		for(buffer_id = 0; buffer_id < node->buffer_count && node->buffer[buffer_id].name[0] != 0; buffer_id++)
 		if(buffer_id == node->buffer_count)
 		{
-			node->buffer_count += 16; 
 			node->buffer = realloc(node->buffer, (sizeof *node->buffer) * node->buffer_count);
-			for(i = node->buffer_count - 16; i < node->buffer_count; i++)
+			for(i = node->buffer_count; i < node->buffer_count + 16; i++)
 				node->buffer[i].name[0] = 0;
+			node->buffer_count = i; 
 		}
 	}	
-	for(i = 0; i < 15 && node->buffer[buffer_id].name[i]; i++)
-		node->buffer[buffer_id].name[i] = node->buffer[buffer_id].name[i];
-	node->buffer[buffer_id].name[i] = 0;
+
 	if(node->buffer[buffer_id].name[0] == 0)
 	{
-		node->buffer[buffer_id].allocated = 256;
-		node->buffer[buffer_id].lines = malloc((sizeof *node->buffer[buffer_id].lines) * 256);
-		for(i = 0; i < 256; i++)
-			node->buffer[buffer_id].lines[i] = NULL;
+		node->buffer[buffer_id].allocated = VS_TEXT_CHUNK_SIZE;
+		node->buffer[buffer_id].text = malloc((sizeof *node->buffer[buffer_id].text) * node->buffer[buffer_id].allocated);
+		node->buffer[buffer_id].length = 0;
 		node->buffer[buffer_id].subscribers = vs_create_subscription_list();
 	}
+	for(i = 0; i < 15 && name[i] != 0; i++)
+		node->buffer[buffer_id].name[i] = name[i];
+	node->buffer[buffer_id].name[i] = 0;
 
 	count =	vs_get_subscript_count(node->head.subscribers);
 	for(i = 0; i < count; i++)
@@ -156,11 +157,8 @@ void callback_send_t_buffer_destroy(void *user, VNodeID node_id, VNMBufferID buf
 		return;
 
 	node->buffer[buffer_id].name[0] = 0;
-	for(i = 0; i < node->buffer[buffer_id].allocated; i++)
-		if(node->buffer[buffer_id].lines[i] != NULL)
-			free(node->buffer[buffer_id].lines[i]);
-	free(node->buffer[buffer_id].lines);
-	vs_destroy_subscription_list(node->buffer[i].subscribers);
+	free(node->buffer[buffer_id].text);
+	vs_destroy_subscription_list(node->buffer[buffer_id].subscribers);
 
 	count =	vs_get_subscript_count(node->head.subscribers);
 	for(i = 0; i < count; i++)
@@ -174,16 +172,20 @@ void callback_send_t_buffer_destroy(void *user, VNodeID node_id, VNMBufferID buf
 static void callback_send_t_buffer_subscribe(void *user, VNodeID node_id, VNMBufferID buffer_id)
 {
 	VSNodeText *node;
-/*	unsigned int i;*/
+	unsigned int i;
 	node = (VSNodeText *)vs_get_node(node_id, V_NT_TEXT);
 	if(node == NULL)
 		return;
 	if(buffer_id >= node->buffer_count || node->buffer[buffer_id].name[0] == 0)
 		return;
 	vs_add_new_subscriptor(node->buffer[buffer_id].subscribers);
-/*	for(i = 0; i < node->buffer[buffer_id].allocated && node->buffer[buffer_id].lines[i] == NULL; i++)
-		verse_send_t_line_insert(node_id, buffer_id, i, 0, node->buffer[buffer_id].lines[i]);
-*/
+	for(i = 0; i < node->buffer[buffer_id].length; i += VN_T_MAX_TEXT_CMD_SIZE)
+	{	
+		if(i + VN_T_MAX_TEXT_CMD_SIZE < node->buffer[buffer_id].length)
+			verse_send_t_text_set(node_id, buffer_id, i, node->buffer[buffer_id].length - i, &node->buffer[buffer_id].text[i]);
+		else
+			verse_send_t_text_set(node_id, buffer_id, i, VN_T_MAX_TEXT_CMD_SIZE, &node->buffer[buffer_id].text[i]);
+	}
 }
 
 static void callback_send_t_buffer_unsubscribe(void *user, VNodeID node_id, VNMBufferID buffer_id)
@@ -197,73 +199,72 @@ static void callback_send_t_buffer_unsubscribe(void *user, VNodeID node_id, VNMB
 	vs_remove_subscriptor(node->buffer[buffer_id].subscribers);
 }
 
-/*
-static void callback_send_t_line_insert(void *user, VNodeID node_id, VNMBufferID buffer_id, uint32 line, uint16 index, char *name)
+static void callback_send_t_text_set(void *user, VNodeID node_id, VNMBufferID buffer_id, uint32 pos, uint32 length, const char *text)
 {
 	VSNodeText *node;
-	unsigned int i, count;
+	unsigned int i, count, text_length;
 	char *buf;
 	node = (VSNodeText *)vs_get_node(node_id, V_NT_TEXT);
 	if(node == NULL)
 		return;
-
 	if(buffer_id >= node->buffer_count || node->buffer[buffer_id].name[0] == 0)
 		return;
 
-	for(i = 0; name[i] != 0; i++);
-	buf = malloc((sizeof *buf) * i);
-	for(i = 0; name[i] != 0; i++)
-		buf[i] = name[i];
-	buf[i] = 0;
+	for(text_length = 0; text[text_length] != 0; text_length++);
 
-	if(node->buffer[buffer_id].lines[node->buffer[buffer_id].allocated - 1] != NULL)
+
+	if(pos > node->buffer[buffer_id].length)
+		pos = node->buffer[buffer_id].length;
+
+	if(pos + length > node->buffer[buffer_id].length)
+		length = node->buffer[buffer_id].length - pos;
+
+	buf = node->buffer[buffer_id].text;
+
+	if(node->buffer[buffer_id].length + text_length - length > node->buffer[buffer_id].allocated)
 	{
-		node->buffer[buffer_id].lines = realloc(node->buffer[buffer_id].lines, (sizeof node->buffer[buffer_id].lines) * (node->buffer[buffer_id].allocated + 256));
-		for(i = node->buffer[buffer_id].allocated; i < node->buffer[buffer_id].allocated + 256; i++)
-			node->buffer[buffer_id].lines[i] = NULL;
-		node->buffer[buffer_id].allocated += 256;
+		buf = realloc(buf, node->buffer[buffer_id].length + text_length - length + VS_TEXT_CHUNK_SIZE);
+		node->buffer[buffer_id].allocated = node->buffer[buffer_id].length + text_length - length + VS_TEXT_CHUNK_SIZE;
 	}
 
-	for(i = node->buffer[buffer_id].allocated - 1; node->buffer[buffer_id].lines[i] == NULL; i--);
-	for(; i != line ; i--)
-		node->buffer[buffer_id].lines[i] = node->buffer[buffer_id].lines[i - 1];
-	node->buffer[buffer_id].lines[i] = buf;
+
+	if(text_length < length)
+	{
+		for(i = 0; text[i] != 0; i++)
+			buf[pos + text_length + i] = buf[pos + length + i];
+
+		for(i = 0; text[i] != 0; i++)
+			buf[pos + i] = text[i];
+	}else
+	{
+		for(i = node->buffer[buffer_id].length; i != pos + text_length; i--)
+			buf[i + text_length - length] = buf[i];
+		buf[i + text_length - length] = buf[i];
+
+		for(i = 0; text[i] != 0; i++)
+			buf[pos + i] = text[i];
+	}
+
+	node->buffer[buffer_id].length += text_length - length;
+
+	if(node->buffer[buffer_id].allocated > VS_TEXT_CHUNK_SIZE * 8 && node->buffer[buffer_id].allocated * 2 > node->buffer[buffer_id].length)
+	{
+		buf = realloc(buf, node->buffer[buffer_id].length + VS_TEXT_CHUNK_SIZE);
+		node->buffer[buffer_id].allocated = node->buffer[buffer_id].length + VS_TEXT_CHUNK_SIZE;
+	}
+
+	node->buffer[buffer_id].length += text_length - length;
+	node->buffer[buffer_id].text = buf;
 
 	count =	vs_get_subscript_count(node->buffer[buffer_id].subscribers);
 	for(i = 0; i < count; i++)
 	{
 		vs_set_subscript_session(node->buffer[buffer_id].subscribers, i);
-		verse_send_t_line_insert(node_id, buffer_id, line, 0, name);
+		verse_send_t_text_set(node_id, buffer_id, pos, length, text);
 	}
 	vs_reset_subscript_session();
 }
 
-static void callback_send_t_line_delete(void *user, VNodeID node_id, VNMBufferID buffer_id, uint32 line)
-{
-	VSNodeText *node;
-	unsigned int i, count;
-	node = (VSNodeText *)vs_get_node(node_id, V_NT_TEXT);
-	if(node == NULL)
-		return;
-
-	if(buffer_id >= node->buffer_count || node->buffer[buffer_id].name[0] == 0)
-		return;
-	if(line >= node->buffer[buffer_id].allocated || node->buffer[buffer_id].lines[line] == NULL)
-		return;
-
-	free(node->buffer[buffer_id].lines[line]);
-	for(i = line + 1; i < node->buffer[buffer_id].allocated && node->buffer[buffer_id].lines[i - 1] != NULL; i++)
-		node->buffer[buffer_id].lines[i - 1] = node->buffer[buffer_id].lines[i];
-
-	count =	vs_get_subscript_count(node->buffer[buffer_id].subscribers);
-	for(i = 0; i < count; i++)
-	{
-		vs_set_subscript_session(node->buffer[buffer_id].subscribers, i);
-		verse_send_t_line_delete(node_id, buffer_id, line);
-	}
-	vs_reset_subscript_session();
-}
-*/
 
 void vs_t_callback_init(void)
 {
@@ -272,9 +273,7 @@ void vs_t_callback_init(void)
 	verse_callback_set(verse_send_t_buffer_destroy,		callback_send_t_buffer_destroy, NULL);
 	verse_callback_set(verse_send_t_buffer_subscribe,	callback_send_t_buffer_subscribe, NULL);
 	verse_callback_set(verse_send_t_buffer_unsubscribe,	callback_send_t_buffer_unsubscribe, NULL);
-/*	verse_callback_set(verse_send_t_line_insert,		callback_send_t_line_insert, NULL);
-	verse_callback_set(verse_send_t_line_delete,		callback_send_t_line_delete, NULL);
-*/
+	verse_callback_set(verse_send_t_text_set,			callback_send_t_text_set, NULL);
 }
 
 #endif
