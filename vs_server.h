df0878b45069a674a4cb4ba4263241eb66d09b74
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-20 09:09:22 +0000

    Changes for R4
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r4-branch@854 49c17534-1d02-dc11-b283-0030488c597c

--- vs_server.h
+++ vs_server.h
@@ -4,21 +4,25 @@
 
 #include <stdlib.h>
 
-extern void	vs_init_connection_storage(void);
-extern void	vs_add_new_connection(VSession session, unsigned int node_id);
-extern void	vs_remove_connection(void);
-extern void	vs_set_next_session(void);
+extern void			vs_init_connection_storage(void);
+extern void			vs_add_new_connection(VSession session, char *name, char *pass, unsigned int node_id);
+extern void			vs_remove_connection(void);
+extern void			vs_set_next_session(void);
 
 typedef void VSSubscriptionList;
 
 extern VSSubscriptionList * vs_create_subscription_list(void);
-extern void	vs_destroy_subscription_list(VSSubscriptionList *list);
-extern void	vs_add_new_subscriptor(VSSubscriptionList *list);
-extern void	vs_remove_subscriptor(VSSubscriptionList *list);
-extern size_t	vs_get_subscript_count(VSSubscriptionList *list);
-extern void	vs_set_subscript_session(VSSubscriptionList *list, unsigned int session);
-extern void	vs_reset_subscript_session(void);
-extern uint32	vs_get_avatar(void);
+extern void			vs_destroy_subscription_list(VSSubscriptionList *list);
+extern void			vs_add_new_subscriptor(VSSubscriptionList *list);
+extern void			vs_remove_subscriptor(VSSubscriptionList *list);
+extern size_t		vs_get_subscript_count(VSSubscriptionList *list);
+extern void			vs_set_subscript_session(VSSubscriptionList *list, unsigned int session);
+extern void			vs_reset_subscript_session(void);
+extern uint32		vs_get_avatar(void);
+extern VSession		vs_get_session(void);
+extern char			*vs_get_user_name(void);
+extern char			*vs_get_user_pass(void);
+
 
 typedef struct {
 	VNodeID			id;
@@ -30,20 +34,20 @@ typedef struct {
 	VSSubscriptionList	*subscribers;
 } VSNodeHead;
 
-extern void		vs_init_node_storage(void);
+extern void			vs_init_node_storage(void);
 extern uint32		vs_add_new_node(VSNodeHead *node, VNodeType type);
 extern VSNodeHead *	vs_get_node(unsigned int node_id, VNodeType type);
 extern VSNodeHead *	vs_get_node_head(unsigned int node_id);
 
-extern void create_node_head(VSNodeHead *node, const char *name, unsigned int owner);
-extern void destroy_node_head(VSNodeHead *node);
-extern void vs_send_node_head(VSNodeHead *node);
+extern void			create_node_head(VSNodeHead *node, const char *name, unsigned int owner);
+extern void			destroy_node_head(VSNodeHead *node);
+extern void			vs_send_node_head(VSNodeHead *node);
 
-extern void vs_o_callback_init(void);
-extern void vs_g_callback_init(void);
-extern void vs_m_callback_init(void);
-extern void vs_b_callback_init(void);
-extern void vs_t_callback_init(void);
-extern void vs_c_callback_init(void);
-extern void vs_h_callback_init(void);
-extern void init_callback_node_storage(void);
+extern void			vs_o_callback_init(void);
+extern void			vs_g_callback_init(void);
+extern void			vs_m_callback_init(void);
+extern void			vs_b_callback_init(void);
+extern void			vs_t_callback_init(void);
+extern void			vs_c_callback_init(void);
+extern void			vs_h_callback_init(void);
+extern void			init_callback_node_storage(void);
