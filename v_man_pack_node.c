6c8c2cce38affd5ac941ba29f08ff69714a7dfea
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-20 09:20:56 +0000

    Connect packing, for R4
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r4-branch@860 49c17534-1d02-dc11-b283-0030488c597c

--- v_man_pack_node.c
+++ v_man_pack_node.c
@@ -11,88 +11,36 @@
 
 #include "verse.h"
 #include "v_cmd_buf.h"
-#include "v_network_que.h"
+#include "v_network_out_que.h"
 #include "v_network.h"
 #include "v_connection.h"
-
-VSession * verse_send_connect(const char *name, const char *pass, const char *address)
-{
-	uint8 *buf;
-	unsigned int buffer_pos = 0;
-	VCMDBufHead *head;
-	VSession con;
-
-	head = v_cmd_buf_allocate(VCMDBS_1500);/* Allocating the buffer */
-	buf = ((VCMDBuffer10 *)head)->buf;
-
-	buffer_pos += vnp_raw_pack_uint8(&buf[buffer_pos], 0);/* Packing the command */
-	#if defined(V_PRINT_SEND_COMMANDS)
-	printf("send: verse_send_connect(name = %s pass = %s address = %p);\n", name, pass, address);
-	#endif
-	buffer_pos += vnp_raw_pack_string(&buf[buffer_pos], name, 16);
-	buffer_pos += vnp_raw_pack_string(&buf[buffer_pos], pass, 16);
-	con = v_con_connect(address);
-	if(con != NULL)
-	{
-		v_cmd_buf_set_unique_size(head, buffer_pos);
-		v_nq_send_buf(v_con_get_network_queue(), head);
-	}else
-		v_cmd_buf_free(head);
-	return con;
-}
+#include "v_encryption.h"
 
 unsigned int v_unpack_connect(const char *buf, unsigned int buffer_length)
 {
-	unsigned int buffer_pos = 0;
-	VSession (* func_connect)(void *user_data, char *name, char *pass, char *address);
-	char name[16];
-	char pass[16];
-	func_connect = v_fs_get_user_func(0);
-	if(buffer_length < 0)
-		return -1;
-	buffer_pos += vnp_raw_unpack_string(&buf[buffer_pos], name, 16, buffer_length - buffer_pos);
-	if(buffer_length < 0 + buffer_pos)
-		return -1;
-	buffer_pos += vnp_raw_unpack_string(&buf[buffer_pos], pass, 16, buffer_length - buffer_pos);
-	if(buffer_length < 0 + buffer_pos)
-		return -1;
-	#if defined(V_PRINT_RECIVE_COMMANDS)
-	printf("receive: verse_send_connect(name = %s pass = %s ); callback = %p\n", name, pass, user_func);
-	#endif
-	if(func_connect != NULL && v_fs_connect_get_address() != NULL)
-		func_connect(v_fs_get_user_data(0), name, pass, v_fs_connect_get_address());
-	return buffer_pos;
-}
-
-VSession * verse_send_connect_accept(VNodeID avatar, const char *address)
-{
-	uint8 *buf;
-	unsigned int buffer_pos = 0;
-	VCMDBufHead *head;
-	VSession con;
-
-	head = v_cmd_buf_allocate(VCMDBS_1500);/* Allocating the buffer */
-	buf = ((VCMDBuffer10 *)head)->buf;
-
-	buffer_pos += vnp_raw_pack_uint8(&buf[buffer_pos], 1);/* Packing the command */
-	#if defined(V_PRINT_SEND_COMMANDS)
-	printf("send: verse_send_connect_accept(avatar = %u address = %p);\n", avatar, address);
-	#endif
-	buffer_pos += vnp_raw_pack_uint32(&buf[buffer_pos], avatar);
-	con = v_con_connect(address);
-	if(con != NULL)
-	{
-		v_cmd_buf_set_unique_size(head, buffer_pos);
-		v_nq_send_buf(v_con_get_network_queue(), head);
-	}else
-		v_cmd_buf_free(head);
-	return con;
+	return -1; /* this command is illegal to send */
 }
 
 unsigned int v_unpack_connect_accept(const char *buf, unsigned int buffer_length)
 {
+	return -1; /* this command is illegal to send */
+}
+
+extern void v_callback_connect_terminate(const char *bye);
+
+unsigned int v_unpack_connect_terminate(const char *buf, unsigned int buffer_length)
+{
 	unsigned int buffer_pos = 0;
-	VSession (* func_connect_accept)(void *user_data, VNodeID avatar, char *address);
+	char bye[512];
+	buffer_pos = vnp_raw_unpack_string(buf, bye, 512, buffer_length);
+	v_callback_connect_terminate(bye);
+	return buffer_pos;
+}
+/*
+unsigned int v_unpack_connect_accept(const char *buf, unsigned int buffer_length)
+{
+	unsigned int buffer_pos = 0;
+	VSession (* func_connect_accept)(void *user_data, VNodeID avatar, char *address, uint8 *host_id);
 	VNodeID avatar;
 	const VNetworkAddress *address;
 	char abuf[32] = "";
@@ -105,13 +53,12 @@ unsigned int v_unpack_connect_accept(const char *buf, unsigned int buffer_length
 	if(address != NULL)
 		v_n_get_address_string(address, abuf);
 	#if defined(V_PRINT_RECIVE_COMMANDS)
-	printf("receive: verse_send_connect_accept(avatar = %u ); callback = %p\n", avatar, user_func);
+	printf("receive: verse_send_connect_accept(avatar = %u, adress = %s, host_id %p); callback = %p\n", avatar, abuf, v_con_get_other_public_key(), user_func);
 	#endif
 	if(func_connect_accept != NULL)
-		func_connect_accept(v_fs_get_user_data(1), avatar, abuf);
-
+		func_connect_accept(v_fs_get_user_data(1), avatar, abuf, v_con_get_other_public_key());
 	return buffer_pos;
-}
+}*/
 
 typedef struct{
 	VNodeID		node_id;
@@ -178,8 +125,9 @@ void verse_send_t_text_set(VNodeID node_id, VNMBufferID buffer_id, uint32 pos, u
 		buffer_pos += vnp_raw_pack_uint8(&buf[buffer_pos], 0);
 	else
 		buffer_pos += vnp_raw_pack_string(&buf[buffer_pos], text, VN_T_MAX_TEXT_CMD_SIZE);
-	v_cmd_buf_set_unique_size(head, buffer_pos);
-	v_nq_send_buf(v_con_get_network_queue(), head);
+	v_cmd_buf_set_unique_address_size(head, buffer_pos);
+	v_cmd_buf_set_size(head, buffer_pos);
+	v_noq_send_buf(v_con_get_network_queue(), head);
 }
 
 void v_call_line(VTempLine *line)
@@ -232,8 +180,10 @@ unsigned int v_unpack_t_text_set(const char *buf, size_t buffer_length)
 				else
 					past->next = line->next;
 				if(line->text != NULL)
+				{
 					free(line->text);
-				free(line->text);
+					line->text = NULL;
+				}
 				past = NULL;
 				line = s->text_temp;
 				s->text_receive_id++;
