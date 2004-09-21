2b7431d6f2ee7473cb0578b30f12fc3e88d76c69
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-21 11:13:21 +0000

    New multi-dimensional curve model implemented
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r4-branch@917 49c17534-1d02-dc11-b283-0030488c597c

--- vs_node_curve.c
+++ vs_node_curve.c
@@ -13,18 +13,19 @@
 #include "vs_server.h"
 
 typedef struct {
-	real64 pre_value;
-	uint32 pre_pos;
-	real64 value;
+	real64 pre_value[4];
+	uint32 pre_pos[4];
+	real64 value[4];
 	real64 pos;
-	real64 post_value;
-	uint32 post_pos;
+	real64 post_value[4];
+	uint32 post_pos[4];
 } VSNKey;
 
 typedef struct {
-	VSNKey		*keys;
-	unsigned int	length;
-	char		name[16];
+	VSNKey				*keys;
+	unsigned int		length;
+	char				name[16];
+	uint8				dimensions;
 	VSSubscriptionList *subscribers;
 } VSNCurve;
 
@@ -63,7 +64,7 @@ void vs_c_subscribe(VSNodeCurve *node)
 		return;
 	for(i = 0; i < node->curve_count; i++)
 		if(node->curves[i].name[0] == 0)
-			verse_send_c_curve_create(node->head.id, i, node->curves[i].name);
+			verse_send_c_curve_create(node->head.id, i, node->curves[i].name, node->curves[i].dimensions);
 
 }
 
@@ -81,7 +82,7 @@ void callback_send_c_unsubscribe(void *user, VNodeID node_id)
 	vs_remove_subscriptor(node->head.subscribers);
 }
 
-static void callback_send_c_curve_create(void *user, VNodeID node_id, VLayerID curve_id, const char *name)
+static void callback_send_c_curve_create(void *user, VNodeID node_id, VLayerID curve_id, const char *name, uint8 dimensions)
 {
 	VSNodeCurve *node;
 	unsigned int i, j, count;
@@ -91,9 +92,12 @@ static void callback_send_c_curve_create(void *user, VNodeID node_id, VLayerID c
 
 	for(i = 0; i < node->curve_count; i++)
 	{
-		for(j = 0; name[j] == node->curves[i].name[j] && name[j] != 0; j++);
-		if(name[j] == node->curves[i].name[j])
-			return;
+		if(curve_id != i)
+		{
+			for(j = 0; name[j] == node->curves[i].name[j] && name[j] != 0; j++);
+			if(name[j] == node->curves[i].name[j])
+				return;
+		}
 	}
 	if(curve_id >= node->curve_count || node->curves[curve_id].name[0] == 0)
 	{
@@ -110,17 +114,36 @@ static void callback_send_c_curve_create(void *user, VNodeID node_id, VLayerID c
 		node->curves[curve_id].length = 64;
 		node->curves[curve_id].keys = malloc((sizeof *node->curves[curve_id].keys) * 64);
 		for(i = 0; name[i] != 0 && i < 64; i++)
-			node->curves[curve_id].keys[i].value = V_REAL64_MAX;			
+			node->curves[curve_id].keys[i].pos = V_REAL64_MAX;			
 		for(i = 0; name[i] != 0 && i < 15; i++)
 			node->curves[curve_id].name[i] = name[i];
 		node->curves[curve_id].name[i] = 0;
+		node->curves[curve_id].dimensions = dimensions;
+	}else if(node->curves[curve_id].dimensions < dimensions)
+	{
+		for(i = 0; i < node->curves[curve_id].length; i++)
+		{
+			if(node->curves[curve_id].keys[i].pos != V_REAL64_MAX)
+			{
+				for(j = node->curves[curve_id].dimensions; j < dimensions; j++)
+				{
+					node->curves[curve_id].keys[i].pre_value[j] = node->curves[curve_id].keys[i].pre_value[0];
+					node->curves[curve_id].keys[i].pre_pos[j] = node->curves[curve_id].keys[i].pre_pos[0];
+					node->curves[curve_id].keys[i].value[j] = node->curves[curve_id].keys[i].value[0];
+					node->curves[curve_id].keys[i].post_value[j] = node->curves[curve_id].keys[i].post_value[0];
+					node->curves[curve_id].keys[i].post_pos[j] = node->curves[curve_id].keys[i].post_pos[0];
+				}
+			}
+		}
+		node->curves[curve_id].dimensions = dimensions;
+		vs_destroy_subscription_list(node->curves[curve_id].subscribers);
+		node->curves[curve_id].subscribers = vs_create_subscription_list();
 	}
-
 	count =	vs_get_subscript_count(node->head.subscribers);
 	for(i = 0; i < count; i++)
 	{
 		vs_set_subscript_session(node->head.subscribers, i);
-		verse_send_c_curve_create(node_id, curve_id, name);
+		verse_send_c_curve_create(node_id, curve_id, name, dimensions);
 	}
 	vs_reset_subscript_session();
 }
@@ -144,34 +167,38 @@ static void callback_send_c_curve_destroy(void *user, VNodeID node_id, VLayerID 
 	vs_reset_subscript_session();
 }
 
-static void callback_send_c_curve_key_set(void *user, VNodeID node_id, VLayerID curve_id, uint32 key_id, real64 pre_value, uint32 pre_pos, real64 value, real64 pos, real64 post_value, uint32 post_pos)
+static void callback_send_c_curve_key_set(void *user, VNodeID node_id, VLayerID curve_id, uint32 key_id, uint8 dimensions, real64 *pre_value, uint32 *pre_pos, real64 *value, real64 pos, real64 *post_value, uint32 *post_pos)
 {
 	VSNodeCurve *node;
 	unsigned int i, count;
 	node = (VSNodeCurve *)vs_get_node(node_id, V_NT_CURVE);
-	if(node == NULL || node->curve_count >= curve_id || node->curves[curve_id].name[0] == 0)
+	if(node == NULL || node->curve_count >= curve_id || node->curves[curve_id].name[0] == 0 || node->curves[curve_id].dimensions != dimensions)
 		return;
 
-	if(node->curves[curve_id].length >= key_id || node->curves[curve_id].keys[key_id].value == V_REAL64_MAX)
+	if(node->curves[curve_id].length >= key_id || node->curves[curve_id].keys[key_id].pos == V_REAL64_MAX)
 	{
-		for(key_id = 0; key_id < node->curves[curve_id].length && node->curves[curve_id].keys[key_id].value != V_REAL64_MAX; key_id++);
+		for(key_id = 0; key_id < node->curves[curve_id].length && node->curves[curve_id].keys[key_id].pos != V_REAL64_MAX; key_id++);
 		if(key_id == node->curves[curve_id].length)
 			for(i = 0; i < 64; i++)
-				node->curves[curve_id].keys[node->curves[curve_id].length++].value = V_REAL64_MAX;
+				node->curves[curve_id].keys[node->curves[curve_id].length++].pos = V_REAL64_MAX;
 	}
-
-	node->curves[curve_id].keys[key_id].pre_value = pre_value;
-	node->curves[curve_id].keys[key_id].pre_pos = pre_pos;
-	node->curves[curve_id].keys[key_id].value = value;
-	node->curves[curve_id].keys[key_id].pos = pos;
-	node->curves[curve_id].keys[key_id].post_value = post_value;
-	node->curves[curve_id].keys[key_id].post_pos = post_pos;
+	for(i = 0; i < dimensions; i++)
+		node->curves[curve_id].keys[key_id].pre_value[i] = pre_value[i];
+	for(i = 0; i < dimensions; i++)
+		node->curves[curve_id].keys[key_id].pre_pos[i] = pre_pos[i];
+	for(i = 0; i < dimensions; i++)
+		node->curves[curve_id].keys[key_id].value[i] = value[i];
+		node->curves[curve_id].keys[key_id].pos = pos;
+	for(i = 0; i < dimensions; i++)
+		node->curves[curve_id].keys[key_id].post_value[i] = post_value[i];
+	for(i = 0; i < dimensions; i++)
+		node->curves[curve_id].keys[key_id].post_pos[i] = post_pos[i];
 
 	count =	vs_get_subscript_count(node->head.subscribers);
 	for(i = 0; i < count; i++)
 	{
 		vs_set_subscript_session(node->head.subscribers, i);
-		verse_send_c_curve_key_set(node_id, curve_id, key_id, pre_value, pre_pos, value, pos, post_value, post_pos);
+		verse_send_c_curve_key_set(node_id, curve_id, key_id, dimensions, pre_value, pre_pos, value, pos, post_value, post_pos);
 	}
 	vs_reset_subscript_session();
 }
@@ -184,10 +211,10 @@ static void callback_send_c_curve_key_destroy(void *user, VNodeID node_id, VLaye
 	if(node == NULL || node->curve_count >= curve_id || node->curves[curve_id].name[0] == 0)
 		return;
 
-	if(node->curves[curve_id].length <= key_id || node->curves[curve_id].keys[key_id].value == V_REAL64_MAX)
+	if(node->curves[curve_id].length <= key_id || node->curves[curve_id].keys[key_id].pos == V_REAL64_MAX)
 		return;
 
-	node->curves[curve_id].keys[key_id].value = V_REAL64_MAX;
+	node->curves[curve_id].keys[key_id].pos = V_REAL64_MAX;
 
 	count =	vs_get_subscript_count(node->head.subscribers);
 	for(i = 0; i < count; i++)
@@ -209,8 +236,8 @@ static void callback_send_c_curve_subscribe(void *user, VNodeID node_id, VLayerI
 	vs_add_new_subscriptor(node->curves[curve_id].subscribers);
 
 	for(i = 0; i < node->curves[curve_id].length; i++)
-		if(node->curves[curve_id].keys[i].value != V_REAL64_MAX)
-			verse_send_c_curve_key_set(node_id, curve_id, i, node->curves[curve_id].keys[i].pre_value, node->curves[curve_id].keys[i].pre_pos, node->curves[curve_id].keys[i].value, node->curves[curve_id].keys[i].pos, node->curves[curve_id].keys[i].post_value, node->curves[curve_id].keys[i].post_pos);
+		if(node->curves[curve_id].keys[i].pos != V_REAL64_MAX)
+			verse_send_c_curve_key_set(node_id, curve_id, i, node->curves[curve_id].dimensions, node->curves[curve_id].keys[i].pre_value, node->curves[curve_id].keys[i].pre_pos, node->curves[curve_id].keys[i].value, node->curves[curve_id].keys[i].pos, node->curves[curve_id].keys[i].post_value, node->curves[curve_id].keys[i].post_pos);
 }
 
 static void callback_send_c_curve_unsubscribe(void *user, VNodeID node_id, VLayerID curve_id)
