4ab04458411dd9ea6a63d9f66f6cd4016deac31f
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-01-17 13:23:33 +0000

    Packet numbering changes
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@1689 49c17534-1d02-dc11-b283-0030488c597c

--- v_network_in_que.c
+++ v_network_in_que.c
@@ -12,7 +12,6 @@
 #include "v_network.h"
 #include "v_pack.h"
 
-#define V_CONNECTON_TIME_OUT 5
 
 #if !defined(V_GENERATE_FUNC_MODE)
 
@@ -28,11 +27,12 @@ void v_niq_clear(VNetInQueue *queue)
 	v_n_get_current_time(&queue->seconds, NULL);
 }
 
-boolean v_niq_time_out(const VNetInQueue *queue)
+uint32 v_niq_time_out(const VNetInQueue *queue)
 {
 	uint32 seconds;
 	v_n_get_current_time(&seconds, NULL);
-	return queue->seconds + V_CONNECTON_TIME_OUT < seconds;
+	printf("time = %u %u %u\n", seconds, queue->seconds, seconds - queue->seconds);
+	return seconds - queue->seconds;
 }
 
 VNetInPacked *v_niq_get(VNetInQueue *queue, size_t *length)
@@ -73,9 +73,16 @@ char *v_niq_store(VNetInQueue *queue, size_t length, unsigned int packet_id)
 	if(packet_id < queue->packet_id)
 		return NULL;
 	
-	while(queue->packet_id < packet_id)
+	while(queue->packet_id != packet_id)
+	{
 		verse_send_packet_nak(queue->packet_id++);
+		if(queue->packet_id == 0)
+			queue->packet_id++;
+
+	}
 	queue->packet_id++;
+	if(queue->packet_id == 0)
+		queue->packet_id++;
 	verse_send_packet_ack(packet_id);
 
 	if(v_niq_temp == NULL)
