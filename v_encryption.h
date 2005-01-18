18c214c059cd16141b87dc6c956a52f8950b7eb8
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-01-18 11:36:33 +0000

    New symbols
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@1698 49c17534-1d02-dc11-b283-0030488c597c

--- v_encryption.h
+++ v_encryption.h
@@ -1,8 +1,17 @@
 
 #define V_ENCRYPTION_LOGIN_KEY_SIZE 512
+#define V_ENCRYPTION_LOGIN_KEY_FULL_SIZE 512 * 3
+#define V_ENCRYPTION_LOGIN_KEY_HALF_SIZE 512 * 2
+
+#define V_ENCRYPTION_LOGIN_PUBLIC_START 512 * 0
+#define V_ENCRYPTION_LOGIN_N_START 512 * 1
+#define V_ENCRYPTION_LOGIN_PRIVATE_START 512 * 2
+
 #define V_ENCRYPTION_DATA_KEY_SIZE 256
 
 extern void v_e_encrypt_data_key_generate(uint8 *to);
 extern void v_e_encrypt_command(uint8 *packet, unsigned int packet_length, const uint8 *command, unsigned int command_length, uint8 *key);
 extern void v_e_dencrypt_data_packet(const uint8 *from, uint8 *to, unsigned int data_length, uint8 *key);
-extern void v_e_encrypt_login(uint8 *data, unsigned int data_length, uint8 *key);
+
+extern void v_e_create_key(uint8 *private_key, uint8 *public_key, uint8 *n);
+extern void v_e_encrypt(uint8 *output, uint8 *data, uint8 *key, uint8 *n);
