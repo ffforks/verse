1cb99fb5cdb66983e4d1eed5bced771ba3e92eb3
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-01-18 11:41:08 +0000

    New return type for time_out()
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@1700 49c17534-1d02-dc11-b283-0030488c597c

--- v_network_in_que.h
+++ v_network_in_que.h
@@ -18,4 +18,4 @@ extern VNetInPacked *v_niq_get(VNetInQueue *queue, size_t *length);
 extern void v_niq_release(VNetInQueue *queue, VNetInPacked *p);
 extern char *v_niq_store(VNetInQueue *queue, size_t length, unsigned int packet_id);
 
-extern boolean v_niq_time_out(const VNetInQueue *queue);
+extern uint32 v_niq_time_out(const VNetInQueue *queue);
