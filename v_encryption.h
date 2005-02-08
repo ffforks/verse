1050bf16b48a2a8a4625be0bab4657d8dbf8638e
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-02-08 14:48:01 +0000

    Changed prototype of encrypt()
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@1797 49c17534-1d02-dc11-b283-0030488c597c

--- v_encryption.h
+++ v_encryption.h
@@ -1,14 +1,13 @@
 
+#include "verse.h"
 
 #define V_ENCRYPTION_LOGIN_KEY_SIZE 256
 #define V_ENCRYPTION_LOGIN_KEY_FULL_SIZE 256 * 3
 #define V_ENCRYPTION_LOGIN_KEY_HALF_SIZE 256 * 2
 
-
 #define V_ENCRYPTION_LOGIN_PUBLIC_START 256 * 0
 #define V_ENCRYPTION_LOGIN_N_START 256 * 1
 #define V_ENCRYPTION_LOGIN_PRIVATE_START 256 * 2
-
 
 #define V_ENCRYPTION_DATA_KEY_SIZE 256
 
@@ -17,4 +16,4 @@ extern void v_e_encrypt_command(uint8 *packet, unsigned int packet_length, const
 extern void v_e_dencrypt_data_packet(const uint8 *from, uint8 *to, unsigned int data_length, uint8 *key);
 
 extern void v_e_create_key(uint8 *private_key, uint8 *public_key, uint8 *n);
-extern void v_e_encrypt(uint8 *output, uint8 *data, uint8 *key, uint8 *n);
+extern void v_e_encrypt(uint8 *output, const uint8 *data, const uint8 *key, const uint8 *key_n);
