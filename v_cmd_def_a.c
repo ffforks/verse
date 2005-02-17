696189c1ecd618a23dc598df8d9dab4eb9031823
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-02-17 12:08:16 +0000

    Reordered parameters, name usually goes early
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@1863 49c17534-1d02-dc11-b283-0030488c597c

--- v_cmd_def_a.c
+++ v_cmd_def_a.c
@@ -13,10 +13,10 @@ void v_gen_audio_cmd_def(void)
 	v_cg_add_param(VCGP_NODE_ID,	"node_id");
 	v_cg_add_param(VCGP_LAYER_ID,	"layer_id");
 	v_cg_add_param(VCGP_END_ADDRESS, NULL);
+	v_cg_add_param(VCGP_NAME,		"name");
 	v_cg_add_param(VCGP_ENUM_NAME,	"VNALayerType");
 	v_cg_add_param(VCGP_ENUM,		"type");
 	v_cg_add_param(VCGP_REAL64,		"frequency");
-	v_cg_add_param(VCGP_NAME,		"name");
 	v_cg_alias(FALSE, "a_layer_destroy", "if(name[0] == 0)", 2, NULL);
 	v_cg_end_cmd();
 
