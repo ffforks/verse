de9eccd85e3aba2ccf89c845187d20d0bf1d5f51
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-09 10:05:41 +0000

    Added timer functions for debugging/profiling
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@2012 49c17534-1d02-dc11-b283-0030488c597c

--- v_util.h
+++ v_util.h
@@ -4,3 +4,12 @@
 
 /* Safe, buffer size limited, string copy. */
 extern char *	v_strlcpy(char *dst, const char *src, size_t size);
+
+typedef struct {
+	uint32 seconds;
+	uint32 fractions;
+} VUtilTimer;
+
+extern void	v_timer_start(VUtilTimer *timer);
+extern double	v_timer_elapsed(const VUtilTimer *timer);
+extern void	v_timer_print(const VUtilTimer *timer);
