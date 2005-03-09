21bd0a7d3d27d7bad2a41186754594438d778abf
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-09 10:11:53 +0000

    Added timer functionality for profiling/optimizing
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@2013 49c17534-1d02-dc11-b283-0030488c597c

--- v_util.c
+++ v_util.c
@@ -4,6 +4,10 @@
 
 #include <stdio.h>
 
+#include "verse_header.h"
+#include "v_network.h"
+#include "v_util.h"
+
 /* Safe string copy. Copies from <src> to <dst>, not using more than <size>
  * bytes of destination space. Always 0-terminates the destination. Returns
  * the beginning of the destination string.
@@ -19,4 +23,25 @@ char * v_strlcpy(char *dst, const char *src, size_t size)
 	*dst = '\0';
 
 	return base;
+}
+
+void v_timer_start(VUtilTimer *timer)
+{
+	v_n_get_current_time(&timer->seconds, &timer->fractions);
+}
+
+double v_timer_elapsed(const VUtilTimer *timer)
+{
+	uint32 cur_seconds, cur_fractions;
+
+	v_n_get_current_time(&cur_seconds, &cur_fractions);
+	return (double)(cur_seconds - timer->seconds) + ((double)cur_fractions - (double)timer->fractions) / (double) 0xffffffff;
+}
+
+void v_timer_print(const VUtilTimer *timer)
+{
+	uint32 cur_seconds, cur_fractions;
+
+	v_n_get_current_time(&cur_seconds, &cur_fractions);
+	printf("%f", (double)(cur_seconds - timer->seconds) + ((double)cur_fractions - (double)timer->fractions) / (double) 0xffffffff);
 }
