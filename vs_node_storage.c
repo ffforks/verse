cf8167961a926ce32f850e2212c3cecd30ffe515
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-11 13:56:38 +0000

    Whitespace edit
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/release-r5-branch@2045 49c17534-1d02-dc11-b283-0030488c597c

--- vs_node_storage.c
+++ vs_node_storage.c
@@ -198,7 +198,6 @@ void callback_send_node_destroy(void *user_data, VNodeID node_id)
 		vs_set_subscript_session(VSNodeStorage.list[type], i);
 		verse_send_node_destroy(node_id);
 	}
-
 	vs_reset_subscript_session();
 }
 
@@ -214,10 +213,10 @@ static void callback_send_node_list(void *user_data, uint32 mask)
 			{
 				if(VSNodeStorage.nodes[j] != NULL && VSNodeStorage.nodes[j]->type == (VNodeType)i)
 				{
-/*					if(VSNodeStorage.nodes[j]->owner == verse_session_get_avatar())
+					if(VSNodeStorage.nodes[j]->owner == verse_session_get_avatar())
 						verse_send_node_create(VSNodeStorage.nodes[j]->id, i, VN_OWNER_MINE);
 					else
-*/						verse_send_node_create(VSNodeStorage.nodes[j]->id, i, VN_OWNER_OTHER);
+						verse_send_node_create(VSNodeStorage.nodes[j]->id, i, VN_OWNER_OTHER);
 				}
 			}
 			vs_add_new_subscriptor(VSNodeStorage.list[i]);
