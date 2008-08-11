d2a92ff452f847c328c3722f604c5996c25ca2f8
Jiri Hnidek <jiri.hnidek@tul.cz>
2008-08-11 15:00:51 +0000

    Added BSD license block to all (not generated) source code files. BSD license block is added to generated files using fprintf().
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4533 49c17534-1d02-dc11-b283-0030488c597c

--- v_cmd_gen.c
+++ v_cmd_gen.c
@@ -1,6 +1,36 @@
 /*
-**
-*/
+ * $Id$ 
+ *
+ * ***** BEGIN BSD LICENSE BLOCK *****
+ *
+ * Copyright (c) 2005-2008, The Uni-Verse Consortium.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END BSD LICENSE BLOCK *****
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -75,6 +105,39 @@ static int v_cg_init(const char *src_path)
 		else
 			f = VCGData.nodes[i];
 		fprintf(f,
+			"/*\n"
+			" * $Id$\n"
+			" *\n"
+			" * ***** BEGIN BSD LICENSE BLOCK *****\n"
+			" *\n"
+			" * Copyright (c) 2005-2008, The Uni-Verse Consortium.\n"
+			" * All rights reserved.\n"
+			" *\n"
+			" * Redistribution and use in source and binary forms, with or without\n"
+			" * modification, are permitted provided that the following conditions are\n"
+			" * met:\n"
+			" *\n"
+			" *  * Redistributions of source code must retain the above copyright\n"
+			" *    notice, this list of conditions and the following disclaimer.\n"
+			" *  * Redistributions in binary form must reproduce the above copyright\n"
+			" *    notice, this list of conditions and the following disclaimer in the\n"
+			" *    documentation and/or other materials provided with the distribution.\n"
+			" *\n"
+			" * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS\n"
+			" * IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED\n"
+			" * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A\n"
+			" * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER\n"
+			" * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,\n"
+			" * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,\n"
+			" * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR\n"
+			" * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF\n"
+			" * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING\n"
+			" * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n"
+			" * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+			" *\n"
+			" * ***** END BSD LICENSE BLOCK *****\n"
+			" *\n"
+			" */\n\n"
 			"/*\n"
 			"** This is automatically generated source code -- do not edit.\n"
 			"** Changes are affected either by editing the corresponding protocol\n"
