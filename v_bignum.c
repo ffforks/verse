ee10dfad19d9c7d3f5a4b7b87ee844850118fc1e
Jiri Hnidek <jiri.hnidek@tul.cz>
2008-08-12 14:49:44 +0000

    svn merge -r 4517:HEAD https://svn.blender.org/svnroot/verse/trunk/verse
    (Added BSD license blocks to this branch too)
    
    
    
    git-svn-id: https://svn.blender.org/svnroot/verse/branches/verse-pam-branch@4534 49c17534-1d02-dc11-b283-0030488c597c

--- v_bignum.c
+++ v_bignum.c
@@ -1,4 +1,38 @@
 /*
+ * $Id$ 
+ *
+ * ***** BEGIN BSD LICENSE BLOCK *****
+ *
+ * Copyright (c) 2005-2008, The Uni-Verse Consortium.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END BSD LICENSE BLOCK *****
+ *
+ */
+
+/*
  * Routines for big (thousands of bits) unsigned integers, and
  * doing simple maths operations on them. Written by Emil Brink.
  * 
@@ -768,7 +802,7 @@ void v_bignum_square_half(VBigDig *x)
 		for(j = i + 1; j < t; j++)
 		{
 /*			printf("computing uv=%X+2*%X*%X+%X\n", w[i + j], x[1 + j], x[1 + i], c);*/
-			uv = x[1 + j] * x[1 + i];
+			uv = ((VBigDigs) x[1 + j]) * ((VBigDigs) x[1 + i]);
 			high = (uv & 0x80000000) != 0;
 			uv *= 2;
 			ouv = uv;	/* Addition below might wrap and generate high bit. */
